2ca071c7d5aa37e2c782b27a3d53c3d772376d1a
Mohamed Ghazal <mohamedghazal1@hotmail.com>
2020-11-20 08:17:17 -0500

    Lecture 4

---
+++ Shared/EmojiMemoryGame.swift
@@ -0,0 +1,29 @@
+//
+//  EmojiMemoryGame.swift
+//  Concentration
+//
+//  Created by Mohamed on 2020-11-15.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame: ObservableObject{
+    @Published private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis: Array<String> = ["🥶","🥵"]
+        return MemoryGame<String>(numberOfPairs: emojis.count) { pairIndex in
+            return emojis[pairIndex]
+        }
+    }
+    
+    //MARK: - Access to the Model
+    var cards: Array<MemoryGame<String>.Card> {
+        model.cards
+    }
+    //MARK: - Intent(s)
+    
+    func choose(card: MemoryGame<String>.Card) {
+        model.choose(card: card)
+    }
+}
